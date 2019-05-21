8068620f1d08a45dd5de7576e4b4d508dc81e252
Max Ploner <git@maxploner.de>
2019-05-21 14:07:35 +0200

    removed pyproj from dependencies

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
 
     # Dependent packages (distributions)
     install_requires=[
-        "pyproj", "geojson", "overpy", "PySocks", "tqdm"
+        "geojson", "overpy", "PySocks", "tqdm"
     ],
 )
